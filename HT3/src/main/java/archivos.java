9ca34c0fcadd90f7e60c1fa9c73348aa509cd2c6
marisaamv <mon19169@uvg.edu.gt>
2020-02-11 19:28:31 -0600

    Se creo el archivo con 3000 numeros random :)

---
+++ HT3/src/main/java/archivos.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Random;
+/**
+ *
+ * @author maris
+ */
+public class archivos {
+    public void crear_archivo(){
+        Random rand = new Random();
+        int w =0;
+        try (PrintWriter salida = new PrintWriter("C:/Users/maris/OneDrive/Escritorio/HT3/HT3/src/main/java/numeros.txt")){
+            while(w<3000){
+                int n = rand.nextInt();
+                salida.print(n + " ");
+                w++;
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
